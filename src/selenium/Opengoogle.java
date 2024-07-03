7eabf8f8af7112946d7312c491acb5f35b133fe9
maha <mahalakshmi10298@gmail.com>
2024-07-03 16:22:56 +0530

    selenium projr

---
+++ src/selenium/Opengoogle.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class Opengoogle {
+	public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver","C:\\Program Files\\Google\\Chrome\\Application");
+
+
+}}
